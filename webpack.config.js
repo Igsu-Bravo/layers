dd6c5e734298ce37b1c6dcb0681650c983862e32
Igsu-Bravo <juan.bravo@futurice.com>
2021-04-02 12:34:46 +0300

    Added frontend files and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env, argv) => {
+  return {
+    // Entry point
+    entry: './src/frontend.tsx',
+    output: {
+        // '/' as the base path for the SPA
+        publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: path.join(__dirname, 'src'),
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      // generate index.html from template
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        loader: 'babel-loader'
+      })
+    ],
+    // Generate source maps for debugging
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+      // 404 fallback
+      historyApiFallback: true
+    }
+  }
+}
